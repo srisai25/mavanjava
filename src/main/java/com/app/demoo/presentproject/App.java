01e63d3212af7e759c0ea9d85158a6e3399789bc
srisai25 <srisaisagarla@gmail.com>
2025-01-15 15:13:48 +0530

    demo maven java

---
+++ src/main/java/com/app/demoo/presentproject/App.java
@@ -0,0 +1,10 @@
+package com.app.demoo.presentproject;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hey from KMIT!!");
+    }
+}
